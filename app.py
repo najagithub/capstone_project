d018ec2c185710f8ae4b24067f68a1fb969023a1
Andrianaja Manarivo <najatrv72@gmail.com>
2023-07-20 08:57:22 +0300

    update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h6> Hello World, my name is Naja updated 2! </h6>'
+    return '<h6> Hello World, my name is Naja! </h6>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
